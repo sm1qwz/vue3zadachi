7c3077f924511332d77fa13a27108b561488d932
sm1qwz <mike_orion@mail.ru>
2023-05-28 14:25:47 +0300

    task9

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,13 +1,16 @@
 <script>
 export default{
 data(){
-  return{
-	obj: {x: 1, y: 2, z: 3}
+  
+},
+methods: {
+  show: function(){
+    alert(new Date().toLocaleDateString());
   }
 }
 }
 </script>
 
 <template>
-<div>{{ obj.x + obj.y + obj.z }}</div>
+<div>{{ show() }}</div>
 </template>
