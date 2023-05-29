f9fd90e7a1c487aa8bcd439975919d6738df751b
sm1qwz <mike_orion@mail.ru>
2023-05-29 19:13:29 +0300

    task20

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,27 +3,29 @@ export default{
   data(){
   return{
     visible: true,
-btnHide: true,
-btnVis: false,
+    visible2: true,
+    visible3: true,
   }
 },
 methods: {
-  hide: function(){
-    this.visible = false;
-    this.btnHide = false;
-    this.btnVis = true;
+  toggle: function(){
+    this.visible = !this.visible;
   },
-  vis: function(){
-    this.visible = true;
-    this.btnHide = true;
-    this.btnVis = false;
+  toggle2: function () {
+    this.visible2 = !this.visible2;
+  },
+  toggle3: function () {
+    this.visible3 = !this.visible3;
   }
 }
 }
 </script>
 
 <template>
-<button @click="hide" v-if="btnHide">Скрыть</button>
-<button @click="vis" v-if="btnVis">Показать</button>
+<button @click="toggle">toggle</button>
 <p v-if="visible">Привет</p>
+<button @click="toggle2">toggle</button>
+<p v-if="visible2">Пока</p>
+<button @click="toggle3">toggle</button>
+<p v-if="visible3">Здравствуй</p>
 </template>
