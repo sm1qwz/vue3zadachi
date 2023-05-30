4d23364c0a2ae812807698d12907ae09716e96ae
sm1qwz <mike_orion@mail.ru>
2023-05-30 14:07:21 +0300

    task23

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,13 +2,18 @@
 export default{
   data(){
   return{
-    isAdmin: true,
+    day: "Пятница",
   }
 },
 }
 </script>
 
 <template>
-<p v-if="isAdmin">Успешно</p>
-<p v-else>Отказано в доступе</p>
+<p v-if="day === 'Понедельник'">Понедельник</p>
+<p v-if="day === 'Вторник'">Вторник</p>
+<p v-if="day === 'Среда'">Среда</p>
+<p v-if="day === 'Четверг'">Четверг</p>
+<p v-if="day === 'Пятница'">Пятница</p>
+<p v-if="day === 'Суббота'">Суббота</p>
+<p v-if="day === 'Воскресенье'">Воскресенье</p>
 </template>
