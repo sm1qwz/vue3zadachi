94935020c2c645f6616b0fc0a0a5a8a84608a9b6
sm1qwz <mike_orion@mail.ru>
2023-05-30 14:03:08 +0300

    task22

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,30 +2,13 @@
 export default{
   data(){
   return{
-    visible: true,
-    visible2: true,
-    visible3: true,
+    isAdmin: true,
   }
 },
-methods: {
-  toggle: function(){
-    this.visible = !this.visible;
-  },
-  toggle2: function () {
-    this.visible2 = !this.visible2;
-  },
-  toggle3: function () {
-    this.visible3 = !this.visible3;
-  }
-}
 }
 </script>
 
 <template>
-<button @click="toggle">toggle</button>
-<p>{{visible ? 'Никита' : 'Киркицкий'}}</p>
-<button @click="toggle1">toggle</button>
-<p>{{visible1 ? 'Клим' : 'Сорокин'}}</p>
-<button @click="toggle2">toggle</button>
-<p>{{visible2 ? 'Мавиле' : 'Бекирова'}}</p>
+<p v-if="isAdmin">Успешно</p>
+<p v-else>Отказано в доступе</p>
 </template>
