c75ea51c11c8f058712d16d466919786e952ce7e
sm1qwz <mike_orion@mail.ru>
2023-05-30 20:04:11 +0300

    task19

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,19 +1,29 @@
 <script>
 export default{
-data(){
+  data(){
   return{
+    visible: true,
+btnHide: true,
+btnVis: false,
   }
 },
 methods: {
+  hide: function(){
+    this.visible = false;
+    this.btnHide = false;
+    this.btnVis = true;
+  },
+  vis: function(){
+    this.visible = true;
+    this.btnHide = true;
+    this.btnVis = false;
+  }
 }
 }
 </script>
 
 <template>
-<p style="color:blueviolet">
-	1
-</p>
-<p style="color:blueviolet">
-	2
-</p>
+<button class="greenstyle" @click="hide" v-if="btnHide">Скрыть</button>
+<button class="greenstyle" @click="vis" v-if="btnVis">Показать</button>
+<p v-if="visible">Привет</p>
 </template>
