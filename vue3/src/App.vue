ae33c3ff99826310c3c076861ecf32232bf64521
sm1qwz <mike_orion@mail.ru>
2023-05-30 18:59:17 +0300

    task35

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,28 +1,46 @@
 <script>
 export default{
-data() {
-	return {
-products: [
-			{
-				id: 1,
-				name: 'product1',
-			},
-			{
-				id: 2,
-				name: 'product2',
-			},
-			{
-				id: 3,
-				name: 'product3',
-			},
-		]
-	}
+data(){
+  return{
+    products: ['PC', 'Monitor', 'Keyboard'],
+  }
 },
 methods: {
+add: function(){
+  this.products.push('Mouse');
+},
+del: function(){
+  this.products.shift();
+},
+del1: function () {
+  this.products.pop();
+},
+splice: function(){
+  this.products.splice(2, 1);
+},
+sort: function(){
+  this.products.sort();
+},
+  reverse: function () {
+    this.products.reverse();
+  },
 }
 }
 </script>
 
 <template>
-<p v-for="prod in products" :key="products.id">{{ prod.name }}</p>
+<ul>
+  <li v-for="elem in products">{{ elem }}</li>
+</ul>
+<button @click="add">Добавить продукт</button>
+<br>
+<button @click="del">Удалить продукт</button>
+<br>
+<button @click="del1">Удалить продукт в конце</button>
+<br>
+<button @click="splice">Удалить предпоследний продукт</button>
+<br>
+<button @click="sort">Отсортировать продукты</button>
+<br>
+<button @click="reverse">Реверс</button>
 </template>
