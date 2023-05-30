eb6e3b238914a6bcb820552734cf5fc548e7b0ce
sm1qwz <mike_orion@mail.ru>
2023-05-30 14:25:00 +0300

    task27

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,18 +2,20 @@
 export default{
 data(){
   return{
-    isAuth: true,
+    items: [1, 2, 3, 4, 5]
   }
 },
 methods: {
-toggle: function(){
-  this.isAuth = false;
-}
 }
 }
 </script>
 
 <template>
-  <button @click="toggle">toggle</button>
-  <p v-show="isAuth">Никита Киркицкий</p>
+   <div v-for="elem in items">{{ elem }}</div>
+  <br>
+  <br>
+  <div v-for="elem in items">{{ elem*elem }}</div>
+  <br>
+  <br>
+  <ul v-for="elem in items">{{ elem }}</ul>
 </template>
