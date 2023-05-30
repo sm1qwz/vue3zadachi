a6953c917b881cc85224dbfecb7d4d3af8005c63
sm1qwz <mike_orion@mail.ru>
2023-05-30 14:42:08 +0300

    task32

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,29 +2,7 @@
 export default{
 data(){
   return{
-    hrefs: [
-			{href: '1.html', text: 'text1'},
-			{href: '2.html', text: 'text2'},
-			{href: '3.html', text: 'text3'},
-		],
-    products: [
-			{
-				name: 'product1',
-				price: 100,
-				quantity: 5
-			},
-			{
-				name: 'product2',
-				price: 200,
-				quantity: 4
-			},
-			{
-				name: 'product3',
-				price: 300,
-				quantity: 3
-			},
-    ]
-  }
+}
 },
 methods: {
 }
@@ -32,16 +10,5 @@ methods: {
 </script>
 
 <template>
-<ul>
-    <li v-for="link in hrefs"><a href="{{ link.href }}">{{ link.text }}</a></li>
-  </ul>
-  <br>
-  <br>
-  <table>
-    <tr v-for="col in products">
-      <td>{{ col.name }}</td>
-      <td>{{ col.price }}</td>
-      <td>{{ col.quantity }}</td>
-    </tr>
-    </table>
+  <p v-for="num in 30"> {{ num }}</p>
 </template>
