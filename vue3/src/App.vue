e326f3179bcf1bc57fe77a70cab1f4d30a9cd042
sm1qwz <mike_orion@mail.ru>
2023-05-31 18:57:13 +0300

    task46

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,33 +2,21 @@
 export default{
 data(){
   return{
-    num: 0,
-    res: 0,
-    sum: 0,
-    num1: 0,
-    num2: 0,
+    checked: true,
+    visible: true,
   }
 },
 methods: {
-  calc:function() {
-    this.res = Math.sqrt(this.num);
-  },
-  summ:function() {
-    this.sum = this.num1 + this.num2;
-  }
-  
+ltoggle: function(){
+  this.checked = false;
+  this.visible = !this.visible;
+}
 }
 }
 </script>
 
 <template>
-<p class="pstyle">{{ res }}</p>
-<input class="inputstyle" v-model="num">
-<button class="greenstyle" @click="calc">work</button>
-<br>
-<p class="pstyle">{{ sum }}</p>
-<input class="inputstyle" v-model="num1">
-<input class="inputstyle" v-model="num2">
-<button class="greenstyle" @click="summ">work</button>
-<br>
+<input class="inputstyle" type="checkbox" v-model="checked">
+<button class="greenstyle" @click="ltoggle">toggle</button>
+<p class="pstyle" v-if="visible">{{ checked ? 'yes' : 'no' }}</p>
 </template>
