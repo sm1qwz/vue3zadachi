219b5da03dbc80742a3e3e3b4f15c563bc9fcb7b
sm1qwz <mike_orion@mail.ru>
2023-05-31 12:34:08 +0300

    task39

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,24 +2,36 @@
 export default{
   data(){
   return{
-    obj: {
-			done: true,
-			selected:  false,
-		},
+    visible: true,
+    styles: {
+      hidden: true,
+    },
   }
 },
 methods: {
-
+setDone:function(){
+  this.styles.hidden = false;
+},
+setTwo: function(){
+  this.styles.hidden = true;
+},
+  toggle: function () {
+    this.visible = !this.visible;
   }
 }
-
+}
 </script>
 
 <template>
-	<p class="obj">
-		1
-	</p>
-	<p class="obj">
-		2
-	</p>
+<button class="greenstyle" @click="toggle">toggle</button>
+<p class="hunter" v-if="visible">Привет</p>
+<p :class="styles">Пока</p>
+<button class="greenstyle" @click="setDone">visible</button>
+<button class="greenstyle" @click="setTwo">hide</button>
 </template>
+
+<style>
+p.hidden{
+  display: none;
+}
+</style>
