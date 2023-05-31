66f5fce80d476e18e02c24c02db037772c06123c
sm1qwz <mike_orion@mail.ru>
2023-05-31 12:41:24 +0300

    task42

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -8,29 +8,11 @@ export default{
   }
 },
 methods: {
-setDone:function(){
-  this.styles.hidden = false;
-},
-setTwo: function(){
-  this.styles.hidden = true;
-},
-  toggle: function () {
-    this.visible = !this.visible;
+
   }
-}
-}
+
 </script>
 
 <template>
-<button class="greenstyle" @click="toggle">toggle</button>
-<p class="{valid: isValid, error: isDisabled}" v-if="visible">Привет</p>
-<p :class="styles">Пока</p>
-<button class="greenstyle" @click="setDone">visible</button>
-<button class="greenstyle" @click="setTwo">hide</button>
-</template>
 
-<style>
-p.hidden{
-  display: none;
-}
-</style>
+</template>
