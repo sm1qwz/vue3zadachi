e27197109b5b1012711895e37c8df2fffd06f63a
sm1qwz <mike_orion@mail.ru>
2023-06-03 15:27:18 +0300

    task51

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,54 +2,28 @@
 export default{
 data(){
   return{
-    selected: 'Среда',
-    selectday: '31',
-    selectmonth: 'Май',
-    selectyear: '2023',
-    options: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    days: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',' 31'],
-    months: ['Январь', 'Февраль',
-      'Март', 'Апрель', 'Май',
-      'Июнь', 'Июль', 'Август', 'Сентябрь',
-      'Октябрь', 'Ноябрь', 'Декабрь'],
-      years: ['2023', '2022',
-      '2021', '2020', '2019',
-      '2018']
+    isDisabled: true,
+    isDisabled1: true,
+    checked: false,
   }
 },
 methods: {
+  toggle: function(){
+    this.isDisabled = false;
+  },
+  toggle1: function(){
+    this.isDisabled1 - false;
+  },
 }
 }
 </script>
 
 <template>
-  <p class="pstyle">Какой сегодня день недели?</p>
-  <select v-model="selected">
-    <option v-for="option in options">{{ option }}</option>
-  </select>
+<input class="inputstyle" type="text" v-bind:disabled="isDisabled">
+<button class="greenstyle" @click="toggle">toggle</button>
 <br>
 <br>
-<p class="pstyle">Какой сегодня день месяца?</p>
-<select v-model="selectday">
-<option v-for="option1 in days">{{ option1 }}</option>
-</select>
+<input class="inputstyle" type="text" v-bind:disabled="isDisabled1">
 <br>
-<br>
-<p class="pstyle">Какой сегодня месяц?</p>
-<select class="hunter" v-model="selectmonth">
-<option v-for="option2 in months">{{ option2 }}</option>
-</select>
-<br>
-<br>
-<p class="pstyle">Какой сегодня год?</p>
-<select class="hunter" v-model="selectyear">
-<option v-for="option3 in years">{{ option3 }}</option>
-</select>
-<br>
-<br>
-<br>
-<p class="pstyle">{{ selected }}</p>
-<p class="pstyle">{{ selectday }}</p>
-<p class="pstyle">{{ selectmonth }}</p>
-<p class="pstyle">{{ selectyear }}</p>
+<input type="checkbox" v-model="checked" @click="toogle1">
 </template>
