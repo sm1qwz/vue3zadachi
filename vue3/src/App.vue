f615d725836dbd85682f461981a460be4ec4a69b
sm1qwz <mike_orion@mail.ru>
2023-06-05 23:35:42 +0300

    task58

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,54 +1,13 @@
 <script>
-export default{
-data(){
-  return{
-    students: [
-      {
-        id: 1,
-        name: 'Nikita Kirkistkiy',
-        mark: 4,
-        age: 19,
-      },
-      {
-        id: 2,
-        name: 'Mavile Bekirova',
-        mark: 5,
-        age: 19,
-      },
-      {
-        id: 3,
-        name: 'Klim Sorokin',
-        mark: 3,
-        age: 18,
-      },
-    ]
+import Name from './components/Name.vue'
+
+export default {
+  components: {
+    Nikita
   }
-},
-methods: {
-  edit(student){
-    student.isEdit = true;
-  },
-  save(student){
-    student.isEdit = false;
-  },
-}
 }
 </script>
 
 <template>
-<table>
-<tr v-for="student in student" :key="student.id"> 
-<template v-if="!student.isEdit">
-  {{ student.name }} {{ student.mark }} {{ student.age }}<button class="greenstyle" @click="edit(student)">edit</button>
-</template>
-<template v-else>
-  <input v-model="student.name">
-  <input v-model="student.mark">
-  <input v-model="student.age">
-  <button class="greenstyle" @click="save(student)">
-      save
-  </button>
-</template>
-</tr>
-</table>
+<Nikita />
 </template>
---
+++ vue3/src/components/Name.vue
@@ -0,0 +1,13 @@
+<script>
+	export default {
+		data() {
+			return {
+				Name: 'Nikita'
+			}
+		}
+	}
+</script>
+
+<template>
+	{{ name }}
+</template>
